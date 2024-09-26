39e185a711f9ebe4785839d93ec09e66aa8e346f
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-26 09:39:48 -0400

    Add project files.

---
+++ Topic 5 - Assignemnt Part 3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Topic_5___Assignemnt_Part_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sam Ahrens 09/26/24
+
+            Console.WriteLine("Welcome to The Impossible Quizz!!");
+            Console.WriteLine("No one can get all 5 questions correct.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.Clear();
+            //Question 1
+            Console.WriteLine("Question 1:");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(""
+
+
+
+
+
+
+        }
+    }
+}
